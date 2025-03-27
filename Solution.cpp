16d372c7edc93fa67b51ee061601a099b0db2cbd
mohammedGasser <mohamedgassermohamede@gmail.com>
2025-03-27 06:23:49 +0200

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -1,3 +1,47 @@
+/////////////////////////////////
+ string s="1-20+2";
+    string arr={0};
+    int temp=0;
+   // int arr_i=0;
+    int register_of_final_result=0;
+    int operand=0;
+    int power_of_ten=0;
+    int sign_before =1;
+    //char flag_operand=0;
+     for(int i=0; i<s.size();i++)
+       {
+     
+            if(s[i]=='+')
+             {
+                register_of_final_result+=sign_before*operand; 
+                sign_before=1;
+                operand=0;
+                power_of_ten=0;
+             }
+           if(s[i]=='-')
+                {
+                    
+                register_of_final_result+=sign_before*operand;
+                 sign_before=-1;
+                  operand=0;
+                power_of_ten=0;  
+            }         
+       if (isdigit(s[i]))
+           {
+                if(power_of_ten>1){
+                    power_of_ten-=1; 
+                }
+                operand=((pow(10,power_of_ten)*operand)+(s[i]-'0'));
+                power_of_ten++;
+            
+             }
+      
+        if(i==s.size()-1)
+        {
+             register_of_final_result+=sign_before*operand;
+        }
+}
+/////////////////////////////////
 class Solution1 {
 public:
     int calculate(string s) {
